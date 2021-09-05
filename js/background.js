10d81605e1510964b6455613ce57043615407db4
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2021-09-06 01:08:24 +0900

    💄 Add a style

--- js/background.js
+++ js/background.js
@@ -2,6 +2,7 @@ const images = ['1.jpg','2.jpg','3.jpg'];
 const chosenImage = Math.ceil(Math.random() * images.length);
 
 const bgImage = document.createElement('img');
+bgImage.classList.add('background');
 
 bgImage.src = `./img/${chosenImage}.jpg`;
-document.body.appendChild(bgImage);
+document.querySelector('.container').appendChild(bgImage);
